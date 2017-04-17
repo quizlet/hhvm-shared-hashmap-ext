55769920bc79963a10b65ba0b4c2f04330148586
Ryan Gordon <ryan@quizlet.com>
2017-04-17 13:48:13 -0700

    Adding in initial test file

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+var_dump(extension_loaded("shared_hashmap"));
+var_dump(function_exists("shhashmap_init"));
+var_dump(function_exists("shhashmap_size"));
+var_dump(function_exists("shhashmap_add"));
+var_dump(function_exists("shhashmap_get"));
+var_dump(shhashmap_init('test'));
+var_dump(shhashmap_add('test', "abc", "123"));
+var_dump(shhashmap_add('test', "def", "456"));
+var_dump(shhashmap_get('test', "abc"));
+var_dump(shhashmap_get('test', "def"));
+
+$sharedHashMap = new SharedHashMap('test2');
+var_dump($sharedHashMap);
+var_dump($sharedHashMap->add("ghi", "789"));
+var_dump($sharedHashMap->add("jkl", "101112"));
+var_dump($sharedHashMap->get("ghi"));
+var_dump($sharedHashMap->get("jkl"));
